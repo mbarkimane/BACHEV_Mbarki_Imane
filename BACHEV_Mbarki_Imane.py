08bf1216eea6b7ca0d1a9d6f50ff7bd35c06bc4e
mbarkimane <mbarkimane02@gmail.com>
2025-12-13 09:29:43 +0100

    Add files via upload

---
+++ BACHEV_Mbarki_Imane.py
@@ -0,0 +1,29 @@
+#Mbarkiimane_M1_BACHEV_11/12/2025
+#Boudaoud Meryem
+#Chekroun Hadjer
+#Tahraoui Fatima
+#Houari Kawther
+# Biology Data Analysis Project
+# Analyse de Séquences ADN avec Python et Pandas
+
+import pandas as pd
+
+# Données initiales
+data = {
+    'Séquence': ['ATGCGTACGTA', 'GCTAGCTAGGCC', 'ATGCGCGTAAGT', 
+                 'TACGATCGTA', 'ATGAAAGGCTT', 'CGTACGTAGC', 'TTAACCGGAT'],
+    'Longueur': [12, 12, 12, 10, 11, 10, 10],
+    'Pourcentage GC': [50, 66.67, 58.33, 40, 45.45, 60, 50]
+}
+
+# 1) Créer et afficher le tableau avec pandas
+df = pd.DataFrame(data)
+print("1) TABLEAU INITIAL")
+print(df)
+print("\n")
+
+# 2) Sélectionner et afficher uniquement la colonne "Longueur"
+print("2) COLONNE 'LONGUEUR'")
+print(df['Longueur'])
+print("\n")
+
